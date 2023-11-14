5593af8d6346338b2f18708c9d02f0eeaccff9f9
info@barwenock.com <info@barwenock.com>
2023-11-14 14:10:37 +0200

    code refactoring code sniffer

--- app/code/Barwenock/VideoImport/Model/VideoImportList.php
+++ app/code/Barwenock/VideoImport/Model/VideoImportList.php
@@ -33,7 +33,7 @@ class VideoImportList
         if (isset($this->providers[$providerCode])) {
             return $this->providers[$providerCode];
         } else {
-            throw new \InvalidArgumentException(sprintf('Provider \'%s\' not implemented in pool.', $providerCode));
+            throw new \InvalidArgumentException(sprintf("Provider '%s' not implemented in pool.", $providerCode));
         }
     }
 }
